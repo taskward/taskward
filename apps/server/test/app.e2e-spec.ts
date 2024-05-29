68d72c21dcb51588c073531034aec7ed5f2ba2be
Bruce <recall4056@gmail.com>
2024-05-30 01:26:32 +0800

    chore: :hammer: update

--- apps/server/test/app.e2e-spec.ts
+++ apps/server/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import type { INestApplication } from '@nestjs/common'
 import type { TestingModule } from '@nestjs/testing'
 import { Test } from '@nestjs/testing'
-import * as request from 'supertest'
+import request from 'supertest'
 
 import { AppModule } from '../src/app.module'
 
