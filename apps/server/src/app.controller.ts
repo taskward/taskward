816ae84ad5899aed14ffb0956444e351ce2b42fb
Bruce <recall4056@gmail.com>
2024-05-30 02:11:58 +0800

    perf(server): :zap: use swc

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, forwardRef, Get, Inject } from '@nestjs/common'
 
-import type { AppService } from './app.service'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject(forwardRef(() => AppService)) private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
