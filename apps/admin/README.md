d71e491bd025398aa6e9f818a856c221cd9826ba
Bruce <recall4056@gmail.com>
2024-05-31 00:06:09 +0800

    feat(admin): :sparkles: init admin project

---
+++ apps/admin/README.md
@@ -0,0 +1,7 @@
+# @taskward/admin
+
+> ✅ Taskward Admin Panel.
+
+## License
+
+[MIT](/LICENSE) License &copy; 2024 Taskward
---
+++ apps/admin/vite.config.ts
@@ -0,0 +1,7 @@
+import ReactSWC from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [ReactSWC()]
+})
