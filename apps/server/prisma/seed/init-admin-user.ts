0ff17f2fa1c602455e55cb428a046160d1e1304b
Bruce <recall4056@gmail.com>
2024-06-03 01:05:48 +0800

    refactor(server): :recycle: use Prisma.validator for data model validation

--- apps/server/prisma/seed/init-admin-user.ts
+++ apps/server/prisma/seed/init-admin-user.ts
@@ -1,7 +1,8 @@
 import { stdout } from 'node:process'
 
 import { hash } from '@node-rs/bcrypt'
-import type { Prisma, PrismaClient } from '@prisma/client'
+import type { PrismaClient } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 
 const ADMIN_USERNAME = 'admin'
 const ADMIN_NICKNAME = 'Bruce'
@@ -10,14 +11,14 @@ const ADMIN_EMAIL = 'recall4056@gmail.com'
 const ADMIN_AVATAR_URL = 'https://avatars.githubusercontent.com/u/62941121?v=4'
 
 export async function initAdminUser(prisma: PrismaClient) {
-  const adminUserInfo = {
+  const adminUserInfo = Prisma.validator<Prisma.UserCreateInput>()({
     username: ADMIN_USERNAME,
     nickName: ADMIN_NICKNAME,
     password: await hash(ADMIN_PASSWORD),
     email: ADMIN_EMAIL,
     avatarUrl: ADMIN_AVATAR_URL,
     enabled: true
-  } satisfies Prisma.UserCreateInput
+  })
 
   const adminUser = await prisma.user.findUnique({
     where: {
