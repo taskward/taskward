a1b299620f4e70587987d0ce59e6de5dae01100f
Bruce <recall4056@gmail.com>
2024-06-12 08:02:16 +0800

    chore: :hammer: update

--- apps/server/src/modules/users/users.controller.ts
+++ apps/server/src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
 
 import { CreateUserDto } from './dto/create-user.dto'
@@ -24,7 +24,19 @@ export class UsersController {
 
   @ApiOperation({ summary: '用户详情 [id]' })
   @Get(':id(\\d+)')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id)
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id)
+  }
+
+  @ApiOperation({ summary: '更新用户' })
+  @Put(':id(\\d+)')
+  update(@Param('id') id: number) {
+    return this.usersService.update(id)
+  }
+
+  @ApiOperation({ summary: '删除用户' })
+  @Delete(':id(\\d+)')
+  remove(@Param('id') id: number) {
+    return this.usersService.remove(id)
   }
 }
