195fef31799d29ebf19a8c2788f509d3a87522e9
Bruce <recall4056@gmail.com>
2024-06-17 00:30:29 +0800

    chore: :hammer: update deps

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: true,
+  reactStrictMode: true,
+  webpack: (config) => config,
+  experimental: {
+    turbo: {
+      resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json']
+    }
+  }
+}
 export default nextConfig
