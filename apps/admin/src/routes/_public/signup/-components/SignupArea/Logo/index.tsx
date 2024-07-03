753ce566f202320105236e91eb905de9f9b9cd00
zhiblcx <1529088692@qq.com>
2024-07-03 21:49:33 +0800

    feat: :sparkles: signup

---
+++ apps/admin/src/routes/_public/signup/-components/SignupArea/Logo/index.tsx
@@ -0,0 +1,23 @@
+import { appConfig } from '@taskward/config'
+
+import logo from '@/assets/images/logo/square.png'
+
+export default function Logo() {
+  return (
+    <Flex
+      className="text-3xl"
+      align="center"
+      justify="center"
+      gap={12}
+    >
+      <img
+        className="rounded-md shadow-md"
+        src={logo}
+        alt="Logo"
+        width={48}
+        height={48}
+      />
+      <span>{appConfig.APP_NAME}</span>
+    </Flex>
+  )
+}
