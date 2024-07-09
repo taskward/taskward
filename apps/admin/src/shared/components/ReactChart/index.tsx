9bc3003bc8c8529480ad2ee307bab8343d7217ae
Bruce <recall4056@gmail.com>
2024-07-10 00:35:58 +0800

    feat(admin): :sparkles: add imperative ref for ReactChart

--- apps/admin/src/shared/components/ReactChart/index.tsx
+++ apps/admin/src/shared/components/ReactChart/index.tsx
@@ -3,54 +3,73 @@ import type { CSSProperties } from 'react'
 
 import type { EChartsInstance } from '@/shared/charts'
 
-interface ReactChartProps {
+export interface ReactChartProps {
   option?: EChartsOption
   initOptions?: EChartsInitOpts
   className?: string
   style?: CSSProperties
 }
 
-export default function ReactChart(props: ReactChartProps) {
-  const { option = {}, initOptions, className, style } = props
+export interface ReactChartRef {
+  instance: EChartsInstance | null
+  chartRef: HTMLDivElement | null
+  resize: () => void
+  setOption: () => void
+}
+
+/**
+ * This component is a wrapper of echarts, which is used to render the chart in react.
+ *
+ * NOTE: Still have some problems need to be solved.
+ * - The animation in resize option is required, otherwise the chart will not resize with animation.
+ * - Warning: [Violation] Added non-passive event listener to a scroll-blocking <some> event. Consider marking event handler as 'passive' to make the page more responsive.
+ * @see https://github.com/apache/echarts/issues/18255
+ */
+const ReactChart = forwardRef<ReactChartRef, ReactChartProps>((props, ref) => {
+  const { option, initOptions, className, style } = props
 
   const themeStore = useThemeStore()
 
-  const [chart, setChart] = useState<EChartsInstance | null>(null)
+  const chartInstance = useRef<EChartsInstance | null>(null)
   const chartRef = useRef<HTMLDivElement | null>(null)
 
   const resize = useCallback(
     () =>
-      chart?.resize({
+      chartInstance.current?.resize({
         animation: {
-          duration: 3000,
-          easing: 'cubicIn'
+          easing: 'cubicIn',
+          duration: 1500
         }
       }),
-    [chart]
+    []
   )
 
-  const setOption = useCallback(() => chart?.setOption(option), [chart, option])
+  const setOption = useCallback(() => chartInstance.current?.setOption(option ?? {}), [option])
 
   /**
    * When the following properties change, the chart will be re-rendered, but before that, the chart will be disposed first.
    * - initOptions
+   * - options
    * - theme
    */
-  useLayoutEffect(() => {
+  useEffect(() => {
     const init = () => {
       const alreadyInit = echarts.getInstanceByDom(chartRef.current!)
       if (!alreadyInit) {
-        const chartInstance = echarts.init(chartRef.current!, themeStore.theme, {
+        chartInstance.current = echarts.init(chartRef.current!, themeStore.theme, {
           renderer: 'svg',
           ...initOptions
         })
-        setChart(chartInstance)
         setOption()
+        resize()
       }
     }
     init()
-    return () => chart?.dispose()
-  }, [chart, initOptions, setOption, themeStore.theme])
+    return () => {
+      chartInstance.current?.dispose()
+      chartInstance.current = null
+    }
+  }, [initOptions, resize, setOption, themeStore.theme])
 
   useEffect(() => {
     window.addEventListener('resize', resize)
@@ -70,10 +89,26 @@ export default function ReactChart(props: ReactChartProps) {
     [className, style]
   )
 
+  useImperativeHandle(
+    ref,
+    () => ({
+      get instance() {
+        return chartInstance.current
+      },
+      get chartRef() {
+        return chartRef.current
+      },
+      resize,
+      setOption
+    }),
+    [resize, setOption]
+  )
+
   return (
     <div
       ref={chartRef}
       {...divProps}
     />
   )
-}
+})
+export default ReactChart
